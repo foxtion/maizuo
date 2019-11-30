b701a11220ef9c5da3cda6f2b1a25c4b1e209c15
foxtion <46696794+foxtion@users.noreply.github.com>
2019-11-30 17:00:06 +0800

    调取摄像头

--- src/page/movie/children/nav_right.vue
+++ src/page/movie/children/nav_right.vue
@@ -2,15 +2,18 @@
     <div id="right">
         <h1>right</h1>
         <Recorder></Recorder>
+        <Camera></Camera>
     </div>
 </template>
 
 <script>
 import Recorder from '../../../components/recorder'
+import Camera from '../../../components/camera'
 export default {
     name:'nav_right',
     components:{
-        Recorder
+        Recorder,
+        Camera
     }
 }
 </script>
